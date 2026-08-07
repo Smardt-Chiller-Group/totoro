fb7e3b6dd05dffd42d3e2729e450761a5597b095
Kim Choy <kinoki.choy@gmail.com>
2026-08-07 22:12:46 +0800

    Add upload functionality

--- src/totoro/blob.py
+++ src/totoro/blob.py
@@ -1,9 +1,12 @@
 import os
+import time
+from datetime import datetime
 
 import click
 import typer
 from azure.storage.blob import BlobServiceClient
 from azure.identity import DefaultAzureCredential
+from azure.core.exceptions import ResourceExistsError
 
 from totoro.validations import validate
 from totoro.settings import load_settings
@@ -11,26 +14,23 @@ from totoro.settings import load_settings
 
 app = typer.Typer()
 config = load_settings()
-blob = config.get('blob')
+blob_config = config.get('blob')
 _credential = DefaultAzureCredential()
 
 def client() -> BlobServiceClient:
     return BlobServiceClient(
-        account_url=blob['endpoint_url'],
+        account_url=blob_config['endpoint_url'],
         credential=_credential,
         max_page_size=500
     )
 
 def get_backups(resource: str, limit: int = 15) -> list:
-    container = client().get_container_client(blob['container'])
+    container = client().get_container_client(blob_config['container'])
     blobs = sorted(
-        container.list_blobs(name_starts_with=f"{blob['prefix']}/{resource}"),
+        container.list_blobs(name_starts_with=f"{blob_config['prefix']}/{resource}"),
         key=lambda b: b.creation_time, reverse=True
     )[:limit]
-    return [
-        f"{os.path.basename(b.name)}"
-        for b in blobs
-    ]
+    return [os.path.basename(b.name) for b in blobs]
 
 @app.callback()
 def callback():
@@ -50,7 +50,47 @@ def list_resources(
     backups_display = '\n'.join(
         [f"[{index+1}]\t{backup}" for index, backup in enumerate(backups)]
     )
-    typer.echo(typer.style(backups_display, dim=True, fg='yellow'))
+    typer.secho(backups_display, dim=True, fg='yellow')
+
+@app.command()
+def upload(
+    resource: str = typer.Argument(..., help='Resource type'),
+    filepath: str = typer.Argument(..., help='File path'),
+):
+    filename = os.path.basename(filepath)
+    file_size = os.path.getsize(filepath)
+    file_size_in_mb = round(file_size / (1024 ** 2), 2)
+
+    container = client().get_container_client(blob_config['container'])
+    blob_client = container.get_blob_client(f"{blob_config['prefix']}/{resource}/{filename}")
+
+    typer.secho(f'Uploading resource: {resource}/{filename} ({file_size_in_mb}MB)', dim=True, fg='green')
+
+    with click.progressbar(length=file_size, empty_char='░', fill_char='▓') as progress_bar:
+        last_seen = 0
+
+        def progress_hook(current, total):
+            nonlocal last_seen
+            progress_bar.update(current - last_seen)
+            last_seen = current
+
+        try:
+            with open(filepath, 'rb') as file:
+                # filenames are timestamped, explictly setting overwrite to false to
+                # guards against accidental overwriting
+                res = blob_client.upload_blob(file, overwrite=False, progress_hook=progress_hook)
+        except ResourceExistsError:
+            typer.echo('\n')
+            typer.secho(f'✖ Upload failed: {blob_client.url} already exists', fg='red', err=True)
+            raise typer.Exit(code=1)
+
+
+    etag = res['etag'].strip('"')
+
+    typer.echo('')
+    typer.secho('✔ Upload complete', dim=True, fg='green', bold=True)
+    typer.secho(f'URL: {blob_client.url}', dim=True, fg='white')
+    typer.secho(f"ETag: {etag}\nLast modified: {res['last_modified']}", dim=True, fg='white')
 
 @app.command()
 def download(
